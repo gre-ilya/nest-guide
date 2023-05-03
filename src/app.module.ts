d36bf1c1e75bf21b5908513a3fb5f0339ed19fab
gre-ilya <ilya.grebenyuk.01@mail.ru>
2023-05-03 13:52:42 +0500

    Added users, implemented REST endpoints.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule],
+  imports: [PrismaModule, ArticlesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })

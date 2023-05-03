189d2cd73ba71527fdadddbe8d92f720b0ce6119
gre-ilya <ilya.grebenyuk.01@mail.ru>
2023-05-03 10:31:31 +0500

    Fixed errors prisma service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismanpxModule } from './prismanpx/prismanpx.module';
 import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismanpxModule],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })

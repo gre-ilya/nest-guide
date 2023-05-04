a1006e1be39fb31a0c18684a362b963bc199beff
gre-ilya <ilya.grebenyuk.01@mail.ru>
2023-05-04 11:43:08 +0500

    Auth done.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, UsersModule],
+  imports: [PrismaModule, ArticlesModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
